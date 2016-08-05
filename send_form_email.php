0ed6b785bc1d16cd412ffc1fd86fb4b0656b1548
jilujoy1991 <jilujoyjinu@gmail.com>
2016-08-05 17:04:57 +0530

    Update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -6,9 +6,9 @@ if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
  
-    $email_to = "you@yourdomain.com";
+    $email_to = "jilujoy1991@gmail.com";
  
-    $email_subject = "Your email subject line";
+    $email_subject = "Enquiry";
  
      
  
@@ -34,13 +34,9 @@ if(isset($_POST['email'])) {
  
     // validation expected data exists
  
-    if(!isset($_POST['first_name']) ||
- 
-        !isset($_POST['last_name']) ||
+    if(!isset($_POST['name']) ||
  
         !isset($_POST['email']) ||
- 
-        !isset($_POST['telephone']) ||
  
         !isset($_POST['comments'])) {
  
@@ -50,13 +46,9 @@ if(isset($_POST['email'])) {
  
      
  
-    $first_name = $_POST['first_name']; // required
- 
-    $last_name = $_POST['last_name']; // required
+    $first_name = $_POST['name']; // required
  
     $email_from = $_POST['email']; // required
- 
-    $telephone = $_POST['telephone']; // not required
  
     $comments = $_POST['comments']; // required
  
@@ -77,12 +69,6 @@ if(isset($_POST['email'])) {
   if(!preg_match($string_exp,$first_name)) {
  
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
- 
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
  
   }
  
@@ -114,11 +100,7 @@ if(isset($_POST['email'])) {
  
     $email_message .= "First Name: ".clean_string($first_name)."\n";
  
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+   $email_message .= "Email: ".clean_string($email_from)."\n";
  
     $email_message .= "Comments: ".clean_string($comments)."\n";
  
