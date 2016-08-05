4179e3704505c9a94103ec20a3ef0c89fd890f62
jilujoy1991 <jilujoyjinu@gmail.com>
2016-08-05 17:54:57 +0530

    Update scriptFile.js

--- js/scriptFile.js
+++ js/scriptFile.js
@@ -61,7 +61,7 @@ function openTab(evt, cityName) {
         // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
         // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function() {
